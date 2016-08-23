4ea2abdce5b07f40630877723b78807ae25a5aab
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-08-23 10:45:24 +0200

    Dodanie prostego widoku

--- GamesRepository/GamesExplorer/App_Start/RouteConfig.cs
+++ GamesRepository/GamesExplorer/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace GamesExplorer
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "",
+                defaults: new { controller = "Game", action = "Games", id = UrlParameter.Optional }
             );
         }
     }
